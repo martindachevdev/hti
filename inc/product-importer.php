39273504588272a14398b0f1961e4b731f8ec436
Martin Dachev <mdachev@developsoft.com>
2024-10-25 15:54:20 +0300

    fix importer subcategories

--- inc/product-importer.php
+++ inc/product-importer.php
@@ -52,8 +52,8 @@ class ProductImporter {
                 $this->import_stats['debug'][] = "Updated product {$existing_product->ID}, deleted {$deleted_count} variations";
                 
                 // Keep existing product data but update necessary fields
-                $product->set_name($product_type); // Update name in case it changed
-                $product->set_status('publish');    // Ensure it's published
+                $product->set_name($product_type);
+                $product->set_status('publish');
                 
                 $this->import_stats['products_updated']++;
             } else {
@@ -69,19 +69,24 @@ class ProductImporter {
             $product->set_sku('');
             $product->set_price(0);
 
-            // Update categories only if provided
-            if (!empty($variations[0][0])) {
-                $category = sanitize_text_field($variations[0][0]);
-                $this->set_product_category($product, $category);
-            }
-            if (!empty($variations[0][1])) {
+            // Set up category hierarchy
+            if (!empty($variations[0][0]) && !empty($variations[0][1])) {
+                $main_category = sanitize_text_field($variations[0][0]);
                 $subcategory = sanitize_text_field($variations[0][1]);
-                $this->set_product_category($product, $subcategory, true);
+                
+                // Create/get main category first
+                $main_term_id = $this->create_or_get_category($main_category);
+                
+                // Create/get subcategory as child of main category
+                $sub_term_id = $this->create_or_get_category($subcategory, $main_term_id);
+                
+                // Set both categories for the product
+                $product->set_category_ids([$main_term_id, $sub_term_id]);
             }
 
-            // Update attributes
+            // Update attributes for variations
             $models = array_map(function ($variation) {
-                return sanitize_text_field($variation[4]);
+                return sanitize_text_field($variation[4]); // Model column
             }, $variations);
 
             $attribute = new WC_Product_Attribute();
@@ -113,29 +118,36 @@ class ProductImporter {
     }
 
     /**
-     * Set product category
+     * Create or get category term
      */
-    private function set_product_category($product, $category_name, $append = false) {
+    private function create_or_get_category($category_name, $parent_id = 0) {
         $term = get_term_by('name', $category_name, 'product_cat');
+        
         if (!$term) {
-            $term = wp_insert_term($category_name, 'product_cat');
-            if (!is_wp_error($term)) {
-                $term_id = $term['term_id'];
-            } else {
-                $this->import_stats['debug'][] = "Error creating category {$category_name}: " . $term->get_error_message();
-                return;
+            $result = wp_insert_term(
+                $category_name,
+                'product_cat',
+                array('parent' => $parent_id)
+            );
+            
+            if (is_wp_error($result)) {
+                $this->import_stats['debug'][] = "Error creating category {$category_name}: " . $result->get_error_message();
+                return 0;
             }
-        } else {
-            $term_id = $term->term_id;
+            
+            return $result['term_id'];
         }
-
-        $current_categories = $product->get_category_ids();
-        if ($append) {
-            $current_categories[] = $term_id;
-        } else {
-            $current_categories = [$term_id];
+        
+        // If the term exists but parent is different, update it
+        if ($term->parent != $parent_id) {
+            wp_update_term(
+                $term->term_id,
+                'product_cat',
+                array('parent' => $parent_id)
+            );
         }
-        $product->set_category_ids($current_categories);
+        
+        return $term->term_id;
     }
 
     /**
@@ -187,13 +199,12 @@ class ProductImporter {
 
         $this->import_stats['debug'][] = "Starting variation processing for product $product_id";
 
-        // Get all product attributes first
-        $models = array_map(function ($variation) {
-            return sanitize_text_field($variation[4]); // Model column
-        }, $variations_data);
-
         // Create variations
-        foreach ($models as $index => $model) {
+        foreach ($variations_data as $index => $variation_data) {
+            $model = sanitize_text_field($variation_data[4]); // Model
+            $description = !empty($variation_data[3]) ? sanitize_text_field($variation_data[3]) : '';
+            $sku = !empty($variation_data[6]) ? sanitize_text_field($variation_data[6]) : '';
+
             // Create variation
             $variation = new WC_Product_Variation();
             $variation->set_parent_id($product_id);
@@ -201,14 +212,12 @@ class ProductImporter {
             $variation->set_price(0);
             $variation->set_regular_price(0);
             
-            // Set SKU if provided
-            if (!empty($variations_data[$index][6])) {
-                $variation->set_sku($variations_data[$index][6]);
+            if ($sku) {
+                $variation->set_sku($sku);
             }
             
-            // Set description if provided
-            if (!empty($variations_data[$index][3])) {
-                $variation->set_description(sanitize_text_field($variations_data[$index][3]));
+            if ($description) {
+                $variation->set_description($description);
             }
             
             // Set attributes
